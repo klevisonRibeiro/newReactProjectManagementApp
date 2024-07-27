e621d418fb41f65fcb887a2a800e135f67f317e2
klevisonRibeiro <klevisonribeiro@live.com>
2024-07-27 12:10:48 -0300

    starting app config

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h3>Running Test</h3>
+      <h3>Running Test --- v2</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
